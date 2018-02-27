384c834db5c12448d9ed892b475165c579da5cf1
acer <icelnw135@gmail.com>
2018-02-27 11:05:14 +0700

    Back Controllter

--- app/src/main/java/chaimonkol/krirk/ac/th/neerfinel/fragment/Calculatefragment.java
+++ app/src/main/java/chaimonkol/krirk/ac/th/neerfinel/fragment/Calculatefragment.java
@@ -3,10 +3,12 @@ package chaimonkol.krirk.ac.th.neerfinel.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import chaimonkol.krirk.ac.th.neerfinel.MainActivity;
 import chaimonkol.krirk.ac.th.neerfinel.R;
 
 /**
@@ -14,6 +16,30 @@ import chaimonkol.krirk.ac.th.neerfinel.R;
  */
 
 public class Calculatefragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toober
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+//        Exchange Controller
+
+
+   }   // Main Method
 
     @Nullable
     @Override
